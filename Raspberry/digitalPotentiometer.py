fba3b6790004fb5ce76637ca1c69ca24a8152551
Rafael Campideli Hoyos <rafahoyos08@gmail.com>
2022-11-23 22:36:41 -0300

    Update digitalPotentiometer.py
    Teoricamente funciona

--- Raspberry/digitalPotentiometer.py
+++ Raspberry/digitalPotentiometer.py
@@ -1,12 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+
 class DigitalPotentiometer:
     def __init__(self, INC_PIN: int, UD_PIN):
         self.INC_PIN = INC_PIN
         self.UD_PIN = UD_PIN
+
+        GPIO.setup(INC_PIN, GPIO.OUT)
+        GPIO.setup(UD_PIN, GPIO.OUT)
+
+        GPIO.output(INC_PIN, GPIO.HIGH)
+
+        # zera o estado do potenciometro digital
+        self.stepDown(100)
+        self.currentStep = 0
     
     # value - de 0 a 100, inversamente proporcional à resistência
     # quanto maior o value, maior é a tensão da saída
-    def setValue(self, value):
-        return not self.invalid
-    
-    def getCommands(self):
-        return self.commands
+    def setValue(self, value: int):
+        stepsToValue = value - self.currentStep
+        if stepsToValue > 0:
+            self.stepUp(stepsToValue)
+        else:
+            self.stepDown(abs(stepsToValue))
+
+    def oneStepUp(self):
+        GPIO.output(self.UD_PIN, GPIO.HIGH)
+        time.sleep(0.01)
+        GPIO.output(self.INC_PIN, GPIO.LOW)
+        time.sleep(0.01)
+        GPIO.output(self.INC_PIN, GPIO.HIGH)
+        time.sleep(0.1)
+
+    def oneStepDown(self):
+        GPIO.output(self.UD_PIN, GPIO.LOW)
+        time.sleep(0.01)
+        GPIO.output(self.INC_PIN, GPIO.LOW)
+        time.sleep(0.01)
+        GPIO.output(self.INC_PIN, GPIO.HIGH)
+        time.sleep(0.1)
+
+    def stepUp(self, howManySteps):
+        for i in range(howManySteps):
+            self.oneStepUp()
+
+    def stepDown(self, howManySteps):
+        for i in range(howManySteps):
+            self.oneStepDown()
